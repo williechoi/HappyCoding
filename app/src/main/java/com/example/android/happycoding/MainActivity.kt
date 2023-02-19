ddaf3cfd90b6197e72bdc1230c1f8af7c714bff8
Hyungsuk Choi <choihyungsuk@gmail.com>
2023-02-19 23:11:51 +0900

    add image to the main screen

--- app/src/main/java/com/example/android/happycoding/MainActivity.kt
+++ app/src/main/java/com/example/android/happycoding/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.android.happycoding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.android.happycoding.ui.theme.HappyCodingTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +28,10 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-                    HappyGreetingWithText("Sam", "James")
+                    HappyGreetingWithImage(
+                        stringResource(R.string.happy_coding_callee),
+                        stringResource(R.string.happy_coding_caller)
+                    )
                 }
             }
         }
@@ -32,23 +39,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HappyGreetingWithText(message: String, from: String) {
-    Column {
+fun HappyGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = "Hey, $message!",
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier.padding(top = 16.dp)
         )
         Text(
             text = "- from $from",
-            fontSize = 24.sp
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 16.dp, end = 16.dp)
+                .align(alignment = Alignment.End)
         )
     }
+}
+
+@Composable
+fun HappyGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+
+    // create a box to overlap image and texts
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        HappyGreetingWithText(message = message, from = from, modifier = modifier)
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun HappyCodingPreview() {
     HappyCodingTheme {
-        HappyGreetingWithText("Mike", "Emma")
+        HappyGreetingWithImage("Mike", "Emma")
     }
 }
